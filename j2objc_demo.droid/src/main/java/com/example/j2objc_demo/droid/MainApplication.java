85482823289f3abc3d2e4ce74d3710cb51470004
simple <simple.continue@gmail.com>
2015-10-22 13:41:44 +0800

    add main menu

---
+++ j2objc_demo.droid/src/main/java/com/example/j2objc_demo/droid/MainApplication.java
@@ -0,0 +1,12 @@
+package com.example.j2objc_demo.droid;
+
+import android.app.Application;
+
+public class MainApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+
+}
